d47863f2c922ed6f95dc174ab217ee113d90e0c3
Ryan Freeman <info@ryanfreeman.io>
2020-05-07 00:17:14 +0100

    Added code to initialise RecyclerView

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.util.Log;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "Movies";
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +21,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         List<MovieEntity> movies = SampleData.getMovies();
-        
-        for(MovieEntity movie : movies) {
+
+        for (MovieEntity movie : movies) {
             Log.i(TAG, movie.toString());
         }
+
+        // initialise the RecyclerView
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        // improves the performance of the RecyclerView if items are of fixed size.
+        recyclerView.setHasFixedSize(true);
+
+        // create and set a LinearLayoutManager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // TODO: add an adapter
     }
 }
