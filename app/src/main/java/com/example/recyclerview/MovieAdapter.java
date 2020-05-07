d93020257f49017c85266cb48da0594b911868ce
Ryan Freeman <info@ryanfreeman.io>
2020-05-08 00:33:35 +0100

    Updated MovieAdapter and movie_thumbs

--- app/src/main/java/com/example/recyclerview/MovieAdapter.java
+++ app/src/main/java/com/example/recyclerview/MovieAdapter.java
@@ -5,32 +5,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
-
-public class MovieAdapter extends RecyclerView.Adapter {
-    private final List<MovieEntity> mMovies;
-
-    // constructor for the dataset
-    public MovieAdapter(List<MovieEntity> mMovies) {
-        this.mMovies = mMovies;
-    }
-
-    // creates new views, called by the LayoutManager
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public MovieAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
-    // Replaces the content of the view, called by the LayoutManager
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MovieAdapter.ViewHolder holder, int position) {
 
     }
 
-    // returns the size of the dataset to the LayoutManager
     @Override
     public int getItemCount() {
-        return mMovies.size();
+        return 0;
     }
 }
---
+++ app/src/main/java/com/example/recyclerview/_MovieAdapter.java
@@ -0,0 +1,66 @@
+package com.example.recyclerview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    private final List<MovieEntity> mMovies;
+    private final Context mContext;
+
+    public MovieAdapter(List<MovieEntity> mMovies, Context mContext) {
+        this.mMovies = mMovies;
+        this.mContext = mContext;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView movieTitle;
+        TextView movieSynopsis;
+        ImageView movieThumbnail;
+
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            movieTitle = view.findViewById(R.id.movie_title);
+            movieSynopsis = view.findViewById(R.id.movie_synopsis);
+            movieThumbnail = view.findViewById(R.id.movie_thumbnail);
+        }
+    }
+
+    public int imageResourceId (Context mContext, String imageName) {
+        return mContext.getResources()
+                .getIdentifier("drawable/" + imageName, null, mContext.getPackageName());
+    }
+
+    @NonNull
+    @Override
+    public MovieAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.movie_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void onBindViewHolder(@NonNull MovieAdapter.ViewHolder holder, int position) {
+        MovieEntity movie = mMovies.get(position);
+
+        holder.movieTitle.setText(String.format("%s (%d)", movie.getTitle(), movie.getYear()));
+        holder.movieSynopsis.setText(movie.getSynopsis());
+        holder.movieThumbnail.setImageResource(imageResourceId(mContext, movie.getThumbnail()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMovies.size();
+    }
+}
