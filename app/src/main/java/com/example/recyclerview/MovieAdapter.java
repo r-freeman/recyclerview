18a2cc83e9f8b62d8bd128e5812985a4e7220a24
Ryan Freeman <info@ryanfreeman.io>
2020-05-07 02:20:02 +0100

    Added comments to MovieAdapter

--- app/src/main/java/com/example/recyclerview/MovieAdapter.java
+++ app/src/main/java/com/example/recyclerview/MovieAdapter.java
@@ -6,17 +6,21 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MovieAdapter extends RecyclerView.Adapter {
+
+    // creates new views, called by the LayoutManager
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
 
+    // Replaces the content of the view, called by the LayoutManager
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
     }
 
+    // returns the size of the dataset to the LayoutManager
     @Override
     public int getItemCount() {
         return 0;
